04cb66ca83a2f7255cad8af488c4314e74cd87c6
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2016-10-06 14:51:07 -0400

    Improved controllers

--- app/zone/index.js
+++ app/zone/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var express = require('express');
+var express    = require('express');
 var controller = require('./zone.controller');
-var router = express.Router();
+var auth       = require('../../auth/auth.service');
+var router     = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated, controller.index);
+router.get('/:id', auth.isAuthenticated, controller.show);
+router.post('/', auth.isAuthenticated, controller.create);
+router.put('/:id', auth.isAuthenticated, controller.update);
+router.delete('/:id', auth.isAuthenticated, controller.destroy);
 
 module.exports = router;
